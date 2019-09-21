004350a8b5004c53b098596a530144026aad79ce
luismy2227 <38512642+luismy2227@users.noreply.github.com>
2019-09-21 16:57:01 -0600

    Blog actualizado

--- class/deleteBlog.php
+++ class/deleteBlog.php
@@ -3,20 +3,27 @@
     include("class_conexion.php");
         try
     {
-
         $blog = $_POST["blog"];
         $conexion = new Conexion();
         $result = ["Error","1"];
-
-        $query = "CALL Funcion_Eliminar_Blog ($blog,@Mensaje , @Error);";
-
+        $query = "CALL Funcion_Eliminar_Blog ($blog,@Mensaje , @Error, @Img);";
         $resultado = $conexion->executeQuery($query);
-        $select = $conexion->executeQuery(' SELECT  @Mensaje , @Error');
+        $select = $conexion->executeQuery(' SELECT  @Mensaje , @Error, @Img');
         $result = $conexion->getRow($select);
+
+        if ($result[2]<>null) {
+            try {
+                $delImg = "../".$result[2];
+                if (file_exists($delImg)) {
+                    unlink($delImg);
+                }
+            } catch (Exception $e) {
+                die("error:". $e->getMessage());
+            }
+        }
 
         echo json_encode($result);
     }
-        catch (Exception $e)
-    {
+    catch (Exception $e){
         die("error:". $e->getMessage());
     }
